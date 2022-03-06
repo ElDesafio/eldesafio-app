01181e0f8d10c158f09a1f37213feb59598b9c0e
lucascurti <lucas.curti@productboard.com>
2022-03-06 18:09:44 +0100

    add nodemon and favicon

--- app/root.tsx
+++ app/root.tsx
@@ -24,6 +24,7 @@ import {
   useTransition,
 } from 'remix';
 
+import favicon from '~/assets/logo.png';
 import { theme } from '~/lib/chakra-ui-pro-theme';
 import styles from '~/styles/styles.css';
 
@@ -84,6 +85,7 @@ const Document = withEmotionCache(
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <link rel="icon" href={favicon} type="image/x-icon" />
           {title ? <title>{title}</title> : null}
           <Meta />
           <Links />
