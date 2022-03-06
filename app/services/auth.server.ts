4d89a245a4600dbc51778b2b4760b07dd04224e5
lucascurti <lucas.curti@productboard.com>
2022-03-06 17:26:52 +0100

    updated callbackURL for auth0

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -1,20 +1,26 @@
-import { User } from "@prisma/client";
-import { redirect } from "@remix-run/server-runtime";
-import { Authenticator } from "remix-auth";
-import { Auth0Strategy } from "remix-auth-auth0";
-import { db } from "./db.server";
-import { sessionStorage } from "./session.server";
+import type { User } from '@prisma/client';
+import { redirect } from '@remix-run/server-runtime';
+import { Authenticator } from 'remix-auth';
+import { Auth0Strategy } from 'remix-auth-auth0';
+
+import { db } from './db.server';
+import { sessionStorage } from './session.server';
 
 // Create an instance of the authenticator, pass a generic with what your
 // strategies will return and will be stored in the session
 export let authenticator = new Authenticator<User | null>(sessionStorage);
+
+const callbackURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/auth/auth0/callback'
+    : 'https://app.eldesafio.org/auth/auth0/callback';
 
 let auth0Strategy = new Auth0Strategy(
   {
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/auth0/callback",
+    callbackURL,
   },
   async ({ accessToken, refreshToken, extraParams, profile }) => {
     // Get the user data from your DB or API using the tokens and profile
@@ -23,9 +29,9 @@ let auth0Strategy = new Auth0Strategy(
       where: { email: profile.emails[0].value },
     });
 
-    if (!user) redirect("/login?error=user_not_found");
+    if (!user) redirect('/login?error=user_not_found');
     return user;
-  }
+  },
 );
 
 authenticator.use(auth0Strategy);
