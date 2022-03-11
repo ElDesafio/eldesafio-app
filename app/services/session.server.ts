85f274d201f7b5ff0d4be777439581dad4cf7f9e
lucascurti <lucas.curti@productboard.com>
2022-03-11 01:18:03 +0100

    set maxAge to 30 days

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -8,6 +8,7 @@ export let sessionStorage = createCookieSessionStorage({
     httpOnly: true,
     secrets: [process.env.COOKIE_SECRET],
     secure: process.env.NODE_ENV === 'production',
+    maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
   },
 });
 
